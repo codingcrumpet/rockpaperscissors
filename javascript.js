fb2ed108ecaf4b53255dde46bd6dd0bc0bd686fe
codingcrumpet <devtucciarone@outlook.com>
2024-03-03 17:09:40 +0000

    changed getComputerChoice function, now functioning as intended

--- javascript.js
+++ javascript.js
@@ -1,3 +1,13 @@
 function getComputerChoice() {
-    
+    const moves = ["rock", "paper", "scissors"]
+    let opponentMove = moves[Math.floor(Math.random() * moves.length)];
+    console.log(opponentMove);
 }
+
+
+/*
+function getComputerChoice() {
+    let moves = ["rock", "paper", "scissors"]
+    let opponentMove = Math.floor(Math.random() * moves.length);
+    return opponentMove;
+*/
