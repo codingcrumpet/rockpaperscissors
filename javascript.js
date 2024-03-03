297ac01ab6aab8748b685ad6ed59462364cbfed5
codingcrumpet <devtucciarone@outlook.com>
2024-03-03 19:34:09 +0000

    added UserPrompt function + if/else if to test user feedback when inputting prompt, also ensured case insensitivity

--- javascript.js
+++ javascript.js
@@ -1,8 +1,21 @@
+let userMovePrompt = prompt("Let's play a game! Choose your weapon: rock, paper, or scissors.") .toLowerCase();
+    if (userMovePrompt == "rock") {
+        console.log("You chose rock");
+    } else if (userMovePrompt == "paper") {
+        console.log("You chose paper");
+    } else if (userMovePrompt == "scissors")
+        console.log("You chose scissors");
+
 function getComputerChoice() {
     const moves = ["rock", "paper", "scissors"]
     let opponentMove = moves[Math.floor(Math.random() * moves.length)];
-    console.log(opponentMove);
+    return opponentMove;
 }
+
+
+
+
+
 
 
 /*
@@ -10,4 +23,14 @@ function getComputerChoice() {
     let moves = ["rock", "paper", "scissors"]
     let opponentMove = Math.floor(Math.random() * moves.length);
     return opponentMove;
+
+
+
+function playRound() {
+    return getComputerChoice {
+    };
+    if 
+}
+
+
 */
